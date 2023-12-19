ec02e6b8ae767480c6fe3cbc9e3611f6e32c53ff
Italo Araujo <italoasouzat.i@gmail.com>
2023-12-19 09:05:20 -0300

    fix: ajustando projeto para roda no mac

--- ios/nubbleTests/nubbleTests.m
+++ ios/nubbleTests/nubbleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NubbleAppTests : XCTestCase
+@interface nubbleTests : XCTestCase
 
 @end
 
-@implementation NubbleAppTests
+@implementation nubbleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
